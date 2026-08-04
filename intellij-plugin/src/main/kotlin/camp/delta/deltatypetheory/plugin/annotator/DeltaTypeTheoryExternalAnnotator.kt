269eeb880a5171304460ae934d77713042fbe505
AmberStarfy <daniel.derevyanko@gmail.com>
2026-08-04 15:19:51 +0200

    non-functional annotation core and plugin amends

---
+++ intellij-plugin/src/main/kotlin/camp/delta/deltatypetheory/plugin/annotator/DeltaTypeTheoryExternalAnnotator.kt
@@ -0,0 +1,9 @@
+package camp.delta.deltatypetheory.plugin.annotator
+
+import com.intellij.lang.annotation.ExternalAnnotator
+
+val x = 1
+val y = 2
+class DeltaTypeTheoryExternalAnnotator : ExternalAnnotator<x,y>() {
+
+}
