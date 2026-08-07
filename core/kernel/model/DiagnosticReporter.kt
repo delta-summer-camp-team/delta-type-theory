40b70b33bb44026b9cf923fa19a53966a0a13f8d
Lera <valeriya.golishnikova@gmail.com>
2026-08-07 16:34:05 +0200

    fixed c2

--- core/kernel/model/DiagnosticReporter.kt
+++
@@ -1,8 +0,0 @@
-package camp.delta.deltatypetheory.core.kernel.model
-
-import camp.delta.deltatypetheory.core.surface.diagnostic.SurfaceDiagnostic
-
-class DiagnosticsReporter {
-
-    val diagnostics: MutableList<SurfaceDiagnostic> = mutableListOf()
-}
--- core/src/main/kotlin/camp/delta/deltatypetheory/core/kernel/model/ElaborationContext.kt
+++ core/src/main/kotlin/camp/delta/deltatypetheory/core/kernel/model/ElaborationContext.kt
@@ -1,15 +1,13 @@
-package camp.delta.deltatypetheory.core.surface.elaborate
-
-
-import camp.delta.deltatypetheory.core.kernel.load.GlobalBinding
+package camp.delta.deltatypetheory.core.kernel.model
 
 class ElaborationContext {
 
-    val globals: MutablMap<String, GlobalBinding> = mutableMapOf()
+    val globals: MutableMap<String, GlobalBinding> = mutableMapOf()
+
     fun addGlobal(binding: GlobalBinding) {
         globals[binding.name.value] = binding
-
     }
+
     fun lookupGlobal(name: String): GlobalBinding? {
         return globals[name]
     }
